5dead9ccc797dbef64899e79954476c3a2e0888e
Manuel1471 <manuel-garccia@hotmail.com>
2022-03-28 23:20:28 -0600

    Update server.go
    This commit add "hello world" to server.

--- server.go
+++ server.go
@@ -1,1 +1,21 @@
 package server
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+)
+
+func HelloHandler(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Hello, Web!\n"))
+}
+
+func main() {
+	fmt.Printf("Starting server at port 2020\n")
+	if err := http.ListenAndServe(":2020", nil); err != nil {
+		log.Fatal(err)
+	}
+	mux := http.NewServeMux()
+	mux.HandleFunc("/hello", HelloHandler)
+
+}
